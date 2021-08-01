9e2059d3145ce9632174cac90a192561222353a9
matthewdinh419 <madi0567@colorado.edu>
2021-08-01 11:18:54 -0600

    bug fixes

--- randomize.py
+++ randomize.py
@@ -4,6 +4,7 @@ pool1 = []
 for line in pool1File:
 	pool1.append(line)
 pool1File.close()
+pool1[-1] += "\n"
 
 # store pool 2 proxies into an array
 pool2File = open("proxies2.txt", "r")
@@ -11,9 +12,11 @@ pool2 = []
 for line in pool2File:
 	pool2.append(line)
 pool2File.close()
+pool2[-1] += "\n"
 
 # add the proxies in order to combined.txt
 resultFile = open("combined.txt", "a")
+resultFile.truncate(0)
 if(len(pool1) >= len(pool2)): # pool 1 has more proxies in it or equal amount
 	for i, proxy in enumerate(pool1):
 		resultFile.write(proxy)
@@ -24,3 +27,4 @@ else: # pool 2 has more proxies in it
 		resultFile.write(proxy)
 		if(i < len(pool1)):
 			resultFile.write(pool1[i])
+resultFile.close()
